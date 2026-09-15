277e0bc1ddf62f2449ff114ae4f1c175360bd9c0
domino <dominik.kis36@gmail.com>
2026-09-15 10:49:17 +0200

    created token for api

---
+++ backend/app/Http/Middleware/CheckToken.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckToken
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        $expectedToken = "KIS-DOMINIK-BOSCH-BEUGRO";
+
+        $token = $request -> $bearerToken();
+
+        if ($token !== $expectedToken)
+        {
+            return response() -> json(["error" => "Hibás token!"], 401);
+        }
+
+        return $next($request);
+    }
+}
